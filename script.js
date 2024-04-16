c485d9b2fabb95418694a3a94a26dd460dd1bdf4
Diego Gomez <diegocor4@gmail.com>
2024-04-16 11:43:55 -0300

    agregadas redes

--- script.js
+++ script.js
@@ -15,33 +15,15 @@ function switchToSpanish(element) {
     console.log(element.textContent.trim()); // Imprime el contenido de element.textContent después de recortar los espacios en blanco
     switch (element.textContent.trim()) {
        
-        case "Projects":
-            element.textContent = "Proyectos";
-            break;
+       
         case "Contact":
             element.textContent = "Contacto";
             break;
-        case "Project 1":
-            element.textContent = "Proyecto 1";
-            break;
-        case "Project 2":
-            element.textContent = "Proyecto 2";
-            break;
-        case "Description of project 1.":
-            element.textContent = "Descripción del proyecto 1.";
-            break;
-        case "Description of project 2.":
-            element.textContent = "Descripción del proyecto 2.";
-            break;
+     
         case "You can contact me via [your email] or on my social media:":
             element.textContent = "Puedes contactarme a través de [tu correo electrónico] o en mis redes sociales:";
             break;
-        case "LinkedIn":
-            element.textContent = "LinkedIn";
-            break;
-        case "GitHub":
-            element.textContent = "GitHub";
-            break;
+       
     }
     // Titulo
     var parrafo = document.getElementById('portfolio-title');
@@ -55,6 +37,10 @@ function switchToSpanish(element) {
      var parrafo = document.getElementById('about-me');
      parrafo.textContent = "Hola soy Diego Desarrollador web y apasionado por la programacion.";
 
+     //proyectos
+     var proyectoTitulo = document.getElementById('projects-title');
+     proyectoTitulo.textContent = "Proyectos";
+
 
     // Cambia el idioma a español
     language = "spanish";
@@ -63,34 +49,14 @@ function switchToSpanish(element) {
 function switchToEnglish(element) {
     switch (element.textContent.trim()) {
     
-        case "Proyectos":
-            element.textContent = "Projects";
-            break;
+        
         case "Contacto":
             element.textContent = "Contact";
-            break;
-        case "Proyecto 1":
-            element.textContent = "Project 1";
-            break;
-        case "Proyecto 2":
-            element.textContent = "Project 2";
-            break;
-        case "Descripción del proyecto 1.":
-            element.textContent = "Description of project 1.";
-            break;
-        case "Descripción del proyecto 2.":
-            element.textContent = "Description of project 2.";
             break;
         case "Puedes contactarme a través de [tu correo electrónico] o en mis redes sociales:":
             element.textContent = "You can contact me via [your email] or on my social media:";
             break;
-        case "LinkedIn":
-            element.textContent = "LinkedIn";
-            break;
-        case "GitHub":
-            element.textContent = "GitHub";
-            break;
-    }
+        
 
     // Titulo
     var parrafo = document.getElementById('portfolio-title');
@@ -103,6 +69,10 @@ function switchToEnglish(element) {
     // acerca de mi parrafo español-->ingles
     var parrafo = document.getElementById('about-me');
     parrafo.textContent = "Hello, I'm Diego, web developer and passionate about programming."
+
+    //proyectos
+    var proyectoTitulo = document.getElementById('projects-title');
+    proyectoTitulo.textContent = "Projects";
 
 
     // Cambia el idioma a inglés
