75fe12be6d692f55b7e519ef6cc2560f17de75ef
Diego Gomez <diegocor4@gmail.com>
2024-04-16 12:23:37 -0300

    completado camio de idioma

--- script.js
+++ script.js
@@ -12,20 +12,6 @@ function changeLanguage() {
 }
 
 function switchToSpanish(element) {
-    console.log(element.textContent.trim()); // Imprime el contenido de element.textContent después de recortar los espacios en blanco
-    switch (element.textContent.trim()) {
-       
-       
-        // case "Contact":
-        //     element.textContent = "Contacto";
-        //     break;
-     
-        // case "You can contact me via [your email] or on my social media:":
-        //     element.textContent = "Puedes contactarme a través de [tu correo electrónico] o en mis redes sociales:";
-        //     break;
-       
-    }
-    // Titulo
     var parrafo = document.getElementById('portfolio-title');
     parrafo.textContent = "Mi Portafolio";
 
@@ -41,22 +27,19 @@ function switchToSpanish(element) {
      var proyectoTitulo = document.getElementById('projects-title');
      proyectoTitulo.textContent = "Proyectos";
 
+     //contactos
+    var contacto = document.getElementById('contact-title');
+    contacto.textContent = "Contacto";
+
+    var parrafo = document.getElementById('contact-p');
+    parrafo.innerHTML = "Puedes <a href='mailto:diegocor4@gmail.com'>enviarme un correo electrónico</a> o encontrarme en mis redes sociales:";
+
 
     // Cambia el idioma a español
     language = "spanish";
 }
 
 function switchToEnglish(element) {
-    switch (element.textContent.trim()) {
-    
-        
-        // case "Contacto":
-        //     element.textContent = "Contact";
-        //     break;
-        // case "Puedes contactarme a través de [tu correo electrónico] o en mis redes sociales:":
-        //     element.textContent = "You can contact me via [your email] or on my social media:";
-        //     break;
-        
 
     // Titulo
     var parrafo = document.getElementById('portfolio-title');
@@ -68,16 +51,24 @@ function switchToEnglish(element) {
 
     // acerca de mi parrafo español-->ingles
     var parrafo = document.getElementById('about-me');
-    parrafo.textContent = "Hello, I'm Diego, web developer and passionate about programming."
+    parrafo.textContent = "Hello, I'm Diego, web developer and passionate about programming.";
 
     //proyectos
     var proyectoTitulo = document.getElementById('projects-title');
     proyectoTitulo.textContent = "Projects";
 
+    //contactos
+    var contacto = document.getElementById('contact-title');
+    contacto.textContent = "Contact";
+
+    var parrafo = document.getElementById('contact-p');
+    parrafo.innerHTML = "You can <a href='mailto:diegocor4@gmail.com'>send me a mail</a> or on my social media:";
+
 
     // Cambia el idioma a inglés
     language = "english";
 }
+
 
 function toggleDarkMode() {
     var body = document.body;
